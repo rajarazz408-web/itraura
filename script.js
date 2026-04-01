0b860cbf9581a1c3b12ae413f8ca7ced6a89a06e
rajarazz408-web <rajarazz408@gmail.com>
2026-04-01 21:49:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+let cart = [];
+
+function addToCart(item){
+  cart.push(item);
+  alert(item + " added to cart");
+}
+
+function buy(item){
+  let num="917618243220";
+  let msg="I want to buy " + item;
+  window.open(`https://wa.me/${num}?text=${encodeURIComponent(msg)}`);
+}
+
+function orderCart(){
+  let num="917618243220";
+  let msg="Order: " + cart.join(", ");
+  window.open(`https://wa.me/${num}?text=${encodeURIComponent(msg)}`);
+}
+
+function filter(cat){
+  if(cat==="all") renderProducts(products);
+  else renderProducts(products.filter(p=>p.category===cat));
+}
+
+function scrollToProducts(){
+  document.getElementById("products").scrollIntoView();
+}
